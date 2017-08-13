8fc02291ffda48fefb441b10283c53324240d75c
colinmipapi <colinjmcfaul@gmail.com>
2017-08-13 05:49:41 -0400

    added wmata data

--- landing/backend.py
+++ landing/backend.py
@@ -1,18 +1,64 @@
 import urllib3
 import simplejson as json
+import http.client, urllib.request, urllib.parse, urllib.error, base64
 
-api_key = 'UMBs_SY2DqqnILtSQJv60g2'
-base_url = 'https://api.reply.io/v1/'
+reply_api_key = 'UMBs_SY2DqqnILtSQJv60g2'
+reply_base_url = 'https://api.reply.io/v1/'
+
+transit_api_key = '3833947c624b4eef9eccf542b4740213'
+transit_base_url = 'api.wmata.com'
 
 def get_campaigns():
 
     item = 'campaigns'
 
-    url = '%s%s?apiKey=%s' % (base_url, item, api_key)
+    url = '%s%s?apiKey=%s' % (reply_base_url, item, reply_api_key)
 
     http = urllib3.PoolManager()
     response = http.request('GET', url)
 
     json_items = json.loads(response.data)
+
+    return json_items
+
+def get_bus_predic(stop_id):
+
+    headers = {
+        # Request headers
+        'api_key': transit_api_key,
+    }
+
+    params = urllib.parse.urlencode({
+        'StopID': stop_id,
+    })
+
+    conn = http.client.HTTPSConnection('api.wmata.com')
+    conn.request("GET", "/NextBusService.svc/json/jPredictions?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+
+    json_items = json.loads(data)
+
+    return json_items
+
+def get_train_predic(station_code):
+
+    headers = {
+        # Request headers
+        'api_key': transit_api_key,
+    }
+
+    url = "/StationPrediction.svc/json/GetPrediction/%s?" % (station_code)
+
+    conn = http.client.HTTPSConnection('api.wmata.com')
+    conn.request("GET", url, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+
+    json_items = json.loads(data)
+
+    print(json_items)
 
     return json_items
--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from landing.backend import get_campaigns
+from landing.backend import get_campaigns, get_train_predic, get_bus_predic
 
 def index(request):
 
+    bus_stops = ['1001454','1001442']
+    train_stops = ['E02','A02']
+
+    buses = []
+    trains = []
+
     campaigns = get_campaigns()
+
+    for stop in bus_stops:
+
+        stop_info = get_bus_predic(stop)
+
+        buses.append(stop_info)
+
+    for station in train_stops:
+
+        station_info = get_train_predic(station)
+
+        trains.append(station_info)
+
+
+    print(buses)
+    print(trains)
+
 
     return render(request,'index.html', {
         'campaigns' : campaigns,
+        'buses' : buses,
+        'trains' : trains,
     })
