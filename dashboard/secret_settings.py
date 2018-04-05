b58322a31bfdb4a247446ca6b22d3141d4a6b60f
colinmipapi <colinjmcfaul@gmail.com>
2018-04-04 22:31:43 -0400

    add gitignore

--- dashboard/secret_settings.py
+++
@@ -1,4 +0,0 @@
-transit_api_key = '3833947c624b4eef9eccf542b4740213'
-maxtrix_api_key = 'AIzaSyDjDg_7TM-NelBZsoKCfKz2xNFO0D1hsHY'
-place_api_key = 'AIzaSyAKqU07QuZf1wezomzykkcSXiqeQNp-gyo'
-SECRET_KEY = 'pfv8gje#7bx#uy56h712dew%tj8fydzyqg+z%6-5=_c35@1ckv'
